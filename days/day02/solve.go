2959275c403d3acbd47c74176bffb66dd44eeb5f
Ben Godfrey <BenJetson@users.noreply.github.com>
2025-11-22 23:20:54 -0500

    Clean up day 2 code.

--- days/day02/solve.go
+++ days/day02/solve.go
@@ -28,10 +28,6 @@ func GetMode(a, b int) Mode {
 	}
 }
 
-// IsSafe determines if a given report is safe.
-// When a report is safe, the bool value is true (ignore the int).
-// When a report is unsafe, the bool value is false and the int reports the
-// index of the level that caused the report to be unsafe.
 func (r Report) IsSafe() bool {
 	var modeForReport Mode
 	for idx := 0; idx < len(r)-1; idx++ {
@@ -68,28 +64,13 @@ func (r Report) IsSafeWithDampener() bool {
 		return true // already safe; nothing to do.
 	}
 
-	// fmt.Println(r)
-
 	for idx := 0; idx < len(r); idx++ {
-		// r2 := slices.Delete(r, idx, idx+1)
-		// r2 := append(r[:idx], r[idx+1:]...)
 		r2 := r.Without(idx)
-
-		// fmt.Println("index", idx,
-		// 	"original", r,
-		// 	"modified", r2,
-		// 	"removed_value", r[idx],
-		// 	"len_original", len(r),
-		// 	"len_modified", len(r2),
-		// )
-
 		if r2.IsSafe() {
-			// fmt.Println("--- ok ---")
 			return true
 		}
 	}
 
-	// fmt.Println("--- bad ---")
 	return false
 }
 
