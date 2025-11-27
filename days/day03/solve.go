f85a8c4ba046402eaea6199ee382e2c1bd682f32
Ben Godfrey <BenJetson@users.noreply.github.com>
2025-11-27 12:48:12 -0500

    first broken attempt at day 3 part 2

--- days/day03/solve.go
+++ days/day03/solve.go
@@ -8,6 +8,9 @@ import (
 	"github.com/BenJetson/aoc-2024/aoc"
 )
 
+// var doInstrPattern = regexp.MustCompile(`do\(\)`)
+// var dontInstrPattern = regexp.MustCompile(`don't\(\)`)
+var modifierInstrPattern = regexp.MustCompile(`do(n')?t\(\)`)
 var mulInstrPattern = regexp.MustCompile(`mul\(([0-9]{1,3}),([0-9]{1,3})\)`)
 
 func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
@@ -15,8 +18,8 @@ func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
 	matches := mulInstrPattern.FindAllStringSubmatch(memory, -1)
 
 	var acc int
+	var a, b int
 	for _, match := range matches {
-		var a, b int
 		if a, err = strconv.Atoi(match[1]); err != nil {
 			return
 		} else if b, err = strconv.Atoi(match[2]); err != nil {
@@ -27,5 +30,46 @@ func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
 	}
 
 	s.Part1.SaveIntAnswer(acc)
+
+	modifierMatches := modifierInstrPattern.FindAllStringIndex(memory, -1)
+	modifierIndices := make([]int, 1, len(modifierMatches)+2)
+	modifierIndices[0] = 0
+	for _, matchIndices := range modifierMatches {
+		modifierIndices = append(modifierIndices, matchIndices[0])
+	}
+	modifierIndices = append(modifierIndices, len(memory))
+
+	var memoryRanges []string
+	for idx := 0; idx < len(modifierIndices)-1; idx++ {
+		a, b = modifierIndices[idx], modifierIndices[idx+1]
+		memoryRanges = append(memoryRanges, memory[a:b])
+	}
+
+	var doAcc int
+	canDo := true
+
+	for _, memoryRange := range memoryRanges {
+		if strings.HasPrefix(memoryRange, "do()") {
+			canDo = true
+		} else if strings.HasPrefix(memoryRange, "don't()") {
+			canDo = false
+		}
+
+		if canDo {
+			matches := mulInstrPattern.FindAllStringSubmatch(memoryRange, -1)
+			for _, match := range matches {
+				if a, err = strconv.Atoi(match[1]); err != nil {
+					return
+				} else if b, err = strconv.Atoi(match[2]); err != nil {
+					return
+				}
+
+				doAcc += a * b
+			}
+		}
+	}
+
+	s.Part2.SaveIntAnswer(doAcc)
+
 	return
 }
