3713a7c140c0cf0a038fb9195f7b647539e04dc5
Ben Godfrey <BenJetson@users.noreply.github.com>
2025-11-28 19:23:14 -0500

    Save code and answer for day 5, part 1.

--- days/day05/solve.go
+++ days/day05/solve.go
@@ -1,7 +1,104 @@
 package day05
 
-import "github.com/BenJetson/aoc-2024/aoc"
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/BenJetson/aoc-2024/aoc"
+	"github.com/BenJetson/aoc-2024/utilities"
+)
+
+type PageNumber int
+
+type Rule struct {
+	Page  PageNumber
+	After PageNumber
+}
+
+func lineToRule(line string) (Rule, error) {
+	values, err := utilities.SliceStringsToIntsLike[PageNumber](
+		strings.Split(line, "|"))
+	if err != nil {
+		return Rule{}, err
+	} else if len(values) != 2 {
+		return Rule{}, fmt.Errorf("unexpected rule values (%d)", len(values))
+	}
+	return Rule{
+		Page:  values[0],
+		After: values[1],
+	}, nil
+}
+
+type UpdatedManual []PageNumber
+
+func (u UpdatedManual) MiddlePage() PageNumber {
+	if len(u)%2 == 0 {
+		panic("unexpected manual with even page count")
+	}
+	idx := (len(u) / 2) // should guaranteed to be odd page count.
+	return u[idx]
+}
+
+func lineToUpdatedManual(line string) (UpdatedManual, error) {
+	pages, err := utilities.SliceStringsToIntsLike[PageNumber](
+		strings.Split(line, ","))
+	if err != nil {
+		return nil, err
+	}
+	return UpdatedManual(pages), nil
+}
 
 func SolvePuzzle(input aoc.Input) (s aoc.Solution, err error) {
+	var rules []Rule
+	var updates []UpdatedManual
+	pastSeparator := false
+
+	for idx, line := range input {
+		switch {
+		case line == "":
+			pastSeparator = true
+		case !pastSeparator:
+			var r Rule
+			r, err = lineToRule(line)
+			if err != nil {
+				err = fmt.Errorf("invalid rule on line %d: %w", idx+1, err)
+				return
+			}
+			rules = append(rules, r)
+		default:
+			var u UpdatedManual
+			u, err = lineToUpdatedManual(line)
+			if err != nil {
+				err = fmt.Errorf("invalid update on line %d: %w", idx+1, err)
+				return
+			}
+			updates = append(updates, u)
+		}
+	}
+
+	// XXX these linear scans are inefficient, but get the job done.
+	var acc int
+	for _, u := range updates {
+		var invalid bool
+		for idx, page := range u {
+			for _, r := range rules {
+				if page == r.Page {
+					otherIdx := slices.Index(u, r.After)
+					if otherIdx >= 0 && idx > otherIdx {
+						invalid = true
+					}
+				}
+			}
+
+		}
+
+		if !invalid {
+			acc += int(u.MiddlePage())
+		}
+	}
+
+	s.Part1.SaveIntAnswer(acc)
+
 	return
 }
--- utilities/conv.go
+++ utilities/conv.go
@@ -2,24 +2,24 @@ package utilities
 
 import "strconv"
 
+func AlphaToIntLike[T ~int](s string) (T, error) {
+	i, err := strconv.Atoi(s)
+	return T(i), err
+}
+
 // SliceStringsToIntsLike takes a slice of strings and attempts to convert
 // to a slice of an integer-like type.
 func SliceStringsToIntsLike[T ~int](numStrs []string) ([]T, error) {
 	if numStrs == nil {
 		return nil, nil
 	}
-
-	var n int
 	var err error
 	nums := make([]T, len(numStrs))
-
 	for idx, s := range numStrs {
-		if n, err = strconv.Atoi(s); err != nil {
+		if nums[idx], err = AlphaToIntLike[T](s); err != nil {
 			return nil, err
 		}
-		nums[idx] = T(n)
 	}
-
 	return nums, nil
 }
 
